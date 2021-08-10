3ba677bf260bc145e438f2682a21ae68c55f8ade
CGreen11 <38218519+CGreen11@users.noreply.github.com>
2021-08-10 09:38:11 +0800

    Update bezier.py

--- bezier.py
+++ bezier.py
@@ -32,16 +32,21 @@ def covert_C_list(pos_list):
 
 
 class Bezier():
-
     @classmethod
     # step为计算精度，越大 点越密集，图线越精细、计算量越大，默认值为流畅上限
-    def bezier(cls, point_left, point_control, point_right, step=10000):
+    def bezier(cls, point_list, step=10000):
         pos_list = []
         for i in range(step + 1):
             t = i / step
-            pos = (cls.bezier_2(point_left[0], point_control[0], point_right[0], t),
-                   cls.bezier_2(point_left[1], point_control[1], point_right[1], t))
-            pos_list.append(pos)
+            pos = None
+            if len(point_list) == 3:
+                pos = (cls.bezier_2(point_list[0][0], point_list[1][0], point_list[2][0], t),
+                       cls.bezier_2(point_list[0][1], point_list[1][1], point_list[2][1], t))
+            elif len(point_list) == 4:
+                pos = (cls.bezier_3(point_list[0][0], point_list[1][0], point_list[2][0], point_list[3][0], t),
+                       cls.bezier_3(point_list[0][1], point_list[1][1], point_list[2][1], point_list[3][1], t))
+            if pos:
+                pos_list.append(pos)
         return pos_list
 
     @classmethod
@@ -51,6 +56,15 @@ class Bezier():
         point_control *= (2 * t * (1 - t))
         point_right *= (t ** 2)
         return point_left + point_control + point_right
+
+    @classmethod
+    # 三阶公式
+    def bezier_3(cls, point_left, point_control_left, point_control_right, point_right, t):
+        b_point_left = point_left * ((1 - t) ** 3)
+        b_point_control_left = point_control_left * (3 * t * ((1 - t) ** 2))
+        b_point_control_right = point_control_right * (3 * (t ** 2) * (1 - t))
+        b_point_right = point_right * (t ** 3)
+        return b_point_left + b_point_control_left + b_point_control_right + b_point_right
 
     @classmethod
     # 官方使用不抗锯齿的绘制完整曲线
@@ -75,24 +89,39 @@ def draw_line(pos_list, m_color=bezier_color):
         pygame.draw.line(screen, m_color, pos_list[0], pos_list[1])
 
 
-# 点 连成竖线，行成区域
-def draw_v_area(pos_list, m_color=bezier_color):
-    for pos in pos_list:
-        draw_line(((pos[0], 0), pos), m_color)
-
-
-# 点 依据轴连成横线，行成区域
-def draw_h_area(pos_list, axis_pos=(screen.get_width() / 2, 0), m_color=bezier_color):
-    # 轴对称
-    symmetry_pos_list = pos_axis_symmetry(pos_list, axis_pos)
-    for i in range(len(symmetry_pos_list)):
-        draw_line((pos_list[i], symmetry_pos_list[i]), m_color)
+# 填充区域 两个点集
+def draw_area(pos_list, symmetry_pos_list, m_color=bezier_color):
+    pos_len = len(symmetry_pos_list)
+    if len(pos_list) > pos_len:
+        pos_len = len(pos_list)
+    for i in range(pos_len):
+        draw_line((get_pos_by_length(i, pos_list), get_pos_by_length(i, symmetry_pos_list)), m_color)
         # 填充间隙
         if i > 0:
             left_top_pos, right_top_pos, right_bottom_pos, left_bottom_pos = covert_C_list(
-                (pos_list[i - 1], symmetry_pos_list[i - 1], symmetry_pos_list[i], pos_list[i]))
+                (get_pos_by_length(i - 1, pos_list), get_pos_by_length(i - 1, symmetry_pos_list),
+                 get_pos_by_length(i, symmetry_pos_list), get_pos_by_length(i, pos_list)))
             gfxdraw.filled_polygon(screen, (left_top_pos, right_top_pos, right_bottom_pos, left_bottom_pos)
                                    , m_color)
+
+
+# 点 依据轴连成横线，行成区域
+def draw_area_axis(pos_list, axis_pos=(screen.get_width() / 2, 0), m_color=bezier_color):
+    # 轴对称
+    symmetry_pos_list = pos_axis_symmetry(pos_list, axis_pos)
+    draw_area(pos_list, symmetry_pos_list, m_color)
+
+
+# 填充区域 单个点集 拆分成两个点集
+def draw_area_self(pos_list, divide_progress=0.5, m_color=bezier_color):
+    draw_area(pos_list[:int(len(pos_list) * divide_progress)], pos_list[:int(len(pos_list) * (1 - divide_progress)):-1],
+              m_color)
+
+
+def get_pos_by_length(i, pos_list):
+    if i >= len(pos_list):
+        i = len(pos_list) - 1
+    return pos_list[i]
 
 
 # 画圆
@@ -102,31 +131,47 @@ def draw_circle(circle_pos, circle_radius, m_color=bezier_color):
     else:
         pygame.draw.circle(screen, m_color, circle_pos, circle_radius)
     # 填充
-    gfxdraw.filled_circle(screen, int(circle_pos[0]), int(circle_pos[1]), int(circle_radius), m_color)
+    # gfxdraw.filled_circle(screen, int(circle_pos[0]), int(circle_pos[1]), int(circle_radius), m_color)
 
 
-# 画矩形
+# 画四边形
 def draw_box(rect, m_color=bezier_color):
-    [left, top, width, height] = rect
-    left_top_pos = (left, top)
-    right_top_pos = (left + width, top)
-    left_bottom_pos = (left, top + height)
-    right_bottom_pos = (left + width, top + height)
     if anti_aliasing:
-        gfxdraw.aapolygon(screen, (left_top_pos, right_top_pos, right_bottom_pos, left_bottom_pos), m_color)
+        # (left_top_pos, right_top_pos, right_bottom_pos, left_bottom_pos)
+        gfxdraw.aapolygon(screen, rect, m_color)
     else:
-        pygame.draw.rect(screen, m_color, [left, top, width, height], 3)
-        # gfxdraw.polygon(screen, (left_top_pos, right_top_pos, right_bottom_pos, left_bottom_pos), m_color)
+        # 画矩形 [left, top, width, height]
+        # pygame.draw.rect(screen, m_color, rect, 3)
+        # 画四边形 (left_top_pos, right_top_pos, right_bottom_pos, left_bottom_pos)
+        gfxdraw.polygon(screen, rect, m_color)
     # 填充
     # gfxdraw.filled_polygon(screen, (left_top_pos, right_top_pos, right_bottom_pos, left_bottom_pos), m_color)
     # gfxdraw.box(screen, rect, m_color)
 
 
+# 填充四边形
+def draw_box_area(rect, m_color=bezier_color):
+    gfxdraw.filled_polygon(screen, rect, m_color)
+
+
+# [left, top, width, height] --> (left_top_pos, right_top_pos, right_bottom_pos, left_bottom_pos)
+def get_box_by_w_h(rect):
+    [left, top, width, height] = rect
+    left_top_pos = (left, top)
+    right_top_pos = (left + width, top)
+    left_bottom_pos = (left, top + height)
+    right_bottom_pos = (left + width, top + height)
+    return (left_top_pos, right_top_pos, right_bottom_pos, left_bottom_pos)
+
+
 # 实心圆点
-def draw_point(pos_list, m_color=bezier_color):
+def draw_point(pos_list, m_color=bezier_color, r_circle=3):
     for pos in pos_list:
         # gfxdraw.pixel(screen, convert_C(pos[0]), convert_C(pos[1]), m_color)
-        gfxdraw.filled_circle(screen, convert_C(pos[0]), convert_C(pos[1]), 3, m_color)
+        pos = (convert_C(pos[0]), convert_C(pos[1]))
+        r_circle = int(r_circle)
+        gfxdraw.filled_circle(screen, pos[0], pos[1], r_circle, m_color)
+        draw_circle((pos[0], pos[1]), r_circle, m_color)
 
 
 # 绘制结束
@@ -207,14 +252,19 @@ def pos_axis_symmetry(pos_list, axis_pos=(screen.get_width() / 2, 0)):
 # 自定义
 def draw_my_bezier(point_left, point_control, point_right):
     # 计算贝塞尔点
-    bezier_pos_list_left = Bezier.bezier(point_left, point_control, point_right, bezier_accuracy)
+    bezier_pos_list_left = Bezier.bezier((point_left, point_control, point_right), bezier_accuracy)
+    # 左左对称
+    bezier_pos_list_left_left = pos_axis_symmetry(bezier_pos_list_left, (point_left[0], 0))
     # 轴对称获得右边
     bezier_pos_list_right = pos_axis_symmetry(bezier_pos_list_left)
+    bezier_pos_list_right_right = pos_axis_symmetry(bezier_pos_list_left_left)
     # 曲线
     draw_lines(bezier_pos_list_left)
+    # draw_lines(bezier_pos_list_left_left)
     draw_lines(bezier_pos_list_right)
+    # draw_lines(bezier_pos_list_right_right)
     # 填充
-    draw_h_area(bezier_pos_list_left)
+    draw_area_axis(bezier_pos_list_left)
     # 绘制左边关键点
     # draw_point((point_left, point_control, point_right), o_circle_color)
 
@@ -250,18 +300,18 @@ def draw_bezier_by_click_pos(mouse_pos):
 
     # 官方 如何填充，振荡
     # 上下对称
-    point_left, point_control, point_right = pos_axis_symmetry((point_left, point_control, point_right),
-                                                               (0, screen.get_height() / 2))
-    Bezier.draw_bezier(screen, point_left, point_control, point_right, bezier_color, bezier_accuracy)
-    point_left, point_control, point_right = pos_axis_symmetry((point_left, point_control, point_right))
-    Bezier.draw_bezier(screen, point_left, point_control, point_right, bezier_color, bezier_accuracy)
-    (circle_pos,) = pos_axis_symmetry((circle_pos,), (0, screen.get_height() / 2))
-    # 外圆
-    draw_circle(circle_pos, out_circle_radius)
-    # 内圆
-    # draw_circle(circle_pos, circle_radius, circle_color)
-    # 内圆使用图片
-    screen.blit(image, (circle_pos[0] - circle_radius, circle_pos[1] - circle_radius))
+    # point_left, point_control, point_right = pos_axis_symmetry((point_left, point_control, point_right),
+    #                                                            (0, screen.get_height() / 2))
+    # Bezier.draw_bezier(screen, point_left, point_control, point_right, bezier_color, bezier_accuracy)
+    # point_left, point_control, point_right = pos_axis_symmetry((point_left, point_control, point_right))
+    # Bezier.draw_bezier(screen, point_left, point_control, point_right, bezier_color, bezier_accuracy)
+    # (circle_pos,) = pos_axis_symmetry((circle_pos,), (0, screen.get_height() / 2))
+    # # 外圆
+    # draw_circle(circle_pos, out_circle_radius)
+    # # 内圆
+    # # draw_circle(circle_pos, circle_radius, circle_color)
+    # # 内圆使用图片
+    # screen.blit(image, (circle_pos[0] - circle_radius, circle_pos[1] - circle_radius))
     #
     submit_draw()
 
@@ -281,14 +331,14 @@ circle_radius_final = 30
 circle_radius = circle_radius_final
 # 图片
 img_file = "./bg_img/no.png"
-image = pygame.image.load(img_file)
+image = pygame.image.load(img_file).convert()
 width, height = image.get_size()
 # 对图片进行缩放
 image = pygame.transform.smoothscale(image, (circle_radius * 2, circle_radius * 2))
 # 抗锯齿
 anti_aliasing = True
 # 精度
-bezier_accuracy = 100
+bezier_accuracy = 500
 # 弹跳速度
 vibration_speed = 0
 vibration_angle_final = math.pi / 2
@@ -316,7 +366,7 @@ if __name__ == '__main__':
                 held_town = True
                 mouse_pos = get_mouse_pos(event.pos)
                 last_mouse_down_pos = mouse_pos
-                submit_draw()
+                # submit_draw()
             elif event.type == pygame.MOUSEBUTTONUP:
                 mouse_pos = get_mouse_pos(event.pos)
                 if held_town:
